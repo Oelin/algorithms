ef937d9b0006c7958d19c2b436a2fd5b551ec565
Oelin <42823429+Oelin@users.noreply.github.com>
2021-01-11 14:16:02 +0000

    Update union-find.js

--- graphs/union-find.js
+++ graphs/union-find.js
@@ -1,1 +1,58 @@
+// problem: given a social network containing N members and a log file containing a sequence of friendships requests, 
+// design an algorithm to determine the earliest time at which all members are connected (i.e. every member is a friend
+// of a friend of a friend ...).
 
+function network() {
+  this.ids = []
+  this.sizes = []
+  this.groups = null
+  
+  // create n new users
+  
+  this.init = function(n) {
+   this.ids = Array(n).fill(0)
+    .map((e, i) => i)
+    
+    this.sizes = Array(n).fill(1)
+    this.groups = n
+  }
+  
+  // find the root of a friend group
+  
+  this.root = function(x) {
+    let parent = this.ids[x]
+    return x == parent ? x : this.root(parent)
+  }
+  
+  // are two users connected?
+  
+  this.find = function(x, y) {
+   return this.root(x) == this.root(y) 
+  }
+  
+  // merge two friend groups
+  
+  this.union = function(x, y) {
+    let rx = this.root(x)
+    let ry = this.root(y)
+    let sx = this.sizes[x]
+    let sy = this.sizes[y]
+    
+    if (rx == ry)
+      return
+    
+    if (sx > sy) {
+      this.ids[rx] = ry
+      this.sizes[ry] += sx
+    } else if (sy > sx) {
+      this.ids[ry] = rx
+      this.sizes[rx] += sy
+    }
+    
+    this.groups --
+  }
+}
+
+function allConnected(network) {
+  return network.groups === 1
+}
