661e5b8998d8fe57225a9f683874011c52ea97b6
Oelin <42823429+Oelin@users.noreply.github.com>
2021-01-19 12:40:17 +0000

    Create possible-paths.js

---
+++ memoization/possible-paths.js
@@ -0,0 +1,31 @@
+// On an r x c grid, how many unique paths exist between cell (0, 0) and cell (x, y)?
+// You can only move down or right.
+// 
+// Write a function paths(r, c, x, y) that returns the solution.
+
+
+// iterative unmemoized
+
+function paths(r, c, x, y) {
+  let q = [[0,0]]
+  let n = 0
+    
+  while (q.length > 0) {
+    let [cx, cy] = q.shift()
+      
+    if (cx != x || cy != y) {
+      if (cx + 1 < w) q.push([cx + 1, cy]) // right
+      if (cy + 1 < h) q.push([cx, cy + 1]) // down
+    } else {
+       n++
+    }
+  }
+    
+  return n
+}
+
+
+// recursive unmemoized
+
+
+// recursive memoized
