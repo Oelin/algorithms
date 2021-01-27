2cc6a4d26720f01d5227e6e2806feb6b9089643f
Oelin <42823429+Oelin@users.noreply.github.com>
2021-01-27 09:45:51 +0000

    Update mergesort.js

--- sorting/mergesort.js
+++ sorting/mergesort.js
@@ -1,9 +1,18 @@
 function merge(a, b) {
   c = []
   
-  while (a.length + b.length) {
+  while (a.length && b.length) {
     c.push((a[0] < b[0] ? a : b).shift())
   }
   
-  return c
+  return [...c, ...(a.length ? a : b)]
+}
+
+
+function mergesort(l) {
+  if (l.length < 2)
+    return l
+  
+  let m = Math.floor(l.length / 2)
+  return merge(mergesort(l.slice(0, m)), mergesort(l.slice(m)))
 }
