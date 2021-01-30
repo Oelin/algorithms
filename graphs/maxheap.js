6303e827a82167f17648e6d3e0ff8c46526b9d12
Oelin <42823429+Oelin@users.noreply.github.com>
2021-01-30 09:11:23 +0000

    Update maxheap.js

--- graphs/maxheap.js
+++ graphs/maxheap.js
@@ -1,21 +1,25 @@
 // I will make this max-heap implementation nicer later
 
 function heap() {
-    let nodes = []
+  let nodes = []
 
-    this.insert = function(value) {
-        nodes.push(value)
-        this.heapify(nodes.length - 1)
+  function heapify(n) {
+    let node
+    let parent
+
+    while ((node = nodes[n]) > nodes[parent = Math.floor((n - 1) / 2)]) {
+      nodes[n] = nodes[parent]
+      nodes[parent] = node
+      n = parent
     }
-
-    this.heapify = function(n) {
-        let node
-        let parent
-
-        while ((node = nodes[n]) > nodes[parent = Math.floor((n - 1) / 2)]) {
-            nodes[n] = nodes[parent]
-            nodes[parent] = node
-            n = parent
-        }
-    }
+  }
+    
+  this.insert = function(value) {
+    nodes.push(value)
+    heapify(nodes.length - 1)
+  }
+    
+  this.toList = function() {
+    return nodes
+  }
 }
